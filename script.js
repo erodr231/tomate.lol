52b19e8ff73a0063dbc8932946d56c1dda248c4d
erodr231 <emro0311@gmail.com>
2026-03-13 14:32:00 -0500

    Add timer logic

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 console.log("testing");
 
 // grabbing all my elements i need to control
+//html elements
 const timer = document.getElementById("timer")
 
 const startBtn = document.getElementById("startBtn");
@@ -11,12 +12,45 @@ const settingsBtn = document.getElementById("settingsBtn");
 const pauseBtn = document.getElementById("pauseBtn");
 const restartBtn = document.getElementById("restartBtn");
 
-// EVENT LISTENERS
-startBtn.addEventListener("click", ()=>{
+//timer elements
+let isPaused = false;
+let timerInterval = null;
+let minutes = 25;
+let seconds = 0;
+
+// FUNCTIONS
+function startTimer(){
     timerMode.classList.add("shrink");
     timerBtns.hidden = false
     timer.hidden = false;
     startBtn.hidden = true;
-})
 
+    timerInterval = setInterval(tick, 1000);
+}
 
+function tick(){
+    if (seconds === 0){ //counting the minutes and seconds
+        minutes--;
+        seconds = 59;
+    } else{
+        seconds--;
+    }
+
+    timer.textContent = `${minutes}:${seconds < 10 ? '0' :''}${seconds}`; //display this on the screen
+}
+
+function toggleTimer(){ // function to toggle resume/pause timer
+    console.log("toggle")
+    if (isPaused){ 
+        //resume
+        timerInterval = setInterval(tick, 1000);
+        isPaused = false;
+    } else {
+        clearInterval(timerInterval);
+        isPaused = true;
+    }
+} 
+
+// EVENT LISTENERS
+startBtn.addEventListener("click", startTimer);
+pauseBtn.addEventListener("click", toggleTimer);
